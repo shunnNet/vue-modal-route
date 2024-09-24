f952a805a541b116aa3c75767479c7c76e252c3b
shunnNet <wendell20904102@gmail.com>
2024-09-24 21:57:50 +0800

    feat: enhance context

--- src/modal/context.ts
+++ src/modal/context.ts
@@ -1,12 +1,21 @@
 export const createContext = () => {
+  // Note: It's may break if trigger new navigation at afterEach
+  // But may work if microtask queue finish all afterEach before next navigation guard queue
   let context: Record<string, any> = {}
+  let nextContext: Record<string, any> = {}
 
   const reset = () => {
-    context = {}
+    context = nextContext
+    nextContext = {}
   }
   const append = (values: Record<string, any>) => {
     Object.entries(values).forEach(([key, value]) => {
       context[key] = value
+    })
+  }
+  const appendNext = (values: Record<string, any>) => {
+    Object.entries(values).forEach(([key, value]) => {
+      nextContext[key] = value
     })
   }
   const get = () => context
@@ -15,5 +24,6 @@ export const createContext = () => {
     get,
     reset,
     append,
+    appendNext,
   }
 }
