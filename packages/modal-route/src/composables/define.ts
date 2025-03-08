799f13b006f4c80c04c4cb860fa2a5d6d49401fa
shunnNet <wendell20904102@gmail.com>
2025-03-08 15:39:30 +0800

    refactor: extract defineXXX to standalone file

---
+++ packages/modal-route/src/composables/define.ts
@@ -0,0 +1,70 @@
+import { RouteRecordRaw } from 'vue-router'
+import { TModalMapItem, TModalQueryRoute } from '../types'
+import { setupModal, useModal } from '../modalRoute'
+
+/**
+ * @experimental
+ * Define a modal route and return functions to setup route and use it.
+ *
+ * User don't need to afraid of changing route name after using this function.
+ *
+ * @example
+ * const ModalA = defineModalRoute<string>(
+ *  'modal-a',
+ *  () => import('./pages/page-single-modal/ModalA.vue'),
+ *  { direct: true },
+ * )
+ *
+ * ModalA.route() // RouteRecordRaw
+ * const { open, close } = ModalA.setup() // Promise<string>
+ *
+ *
+ */
+export const defineModalRoute = <ReturnValue = any>(
+  path: string,
+  component: RouteRecordRaw['component'],
+  options: {
+    name?: string
+    direct?: boolean
+  } = {},
+) => {
+  // TODO: parse path to name like nuxt did
+  const name = path
+  return {
+    name: options.name || name,
+    route: (children?: RouteRecordRaw[]) => ({
+      name,
+      path,
+      component,
+      meta: {
+        modal: true,
+        direct: options.direct,
+      },
+      children,
+    } as RouteRecordRaw),
+    setup: (options?: TModalMapItem['options']) => setupModal<ReturnValue>(name, options),
+    use: () => useModal<ReturnValue>(name),
+  }
+}
+
+/**
+ * @experimental
+ * Define a modal query route and return functions to setup route and use it.
+ *
+ * User don't need to afraid of changing route name after using this function.
+ */
+export const defineModalQueryRoute = <ReturnValue = any>(
+  path: string,
+  component: TModalQueryRoute['component'],
+) => {
+  const name = path
+  return {
+    name,
+    route: () => ({
+      name,
+      component,
+    }),
+    setup: (options?: TModalMapItem['options']) => setupModal<ReturnValue>(name, options),
+    use: () => useModal<ReturnValue>(name),
+  }
+}
--- packages/modal-route/src/index.ts
+++ packages/modal-route/src/index.ts
@@ -1,6 +1,7 @@
 export * from './modalRoute'
 export * from './types'
 export * from './helpers'
+export * from './composables/define'
 
 export { default as ModalRouterView } from './ModalRouterView'
 export { default as ModalPathView } from './ModalPathView'
